b64854906f2301761161329db33495f5a86e824b
Michał Jarek <miciu77@gmail.com>
2023-02-09 14:25:39 +0100

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CLI-aplikacja
+
+Link do zdjęć w wykonanych zadań:
+https://drive.google.com/drive/folders/1WxhvtQdeeo5zIAffGZWBlkhV9SJjLmEE?usp=sharing
---
+++ contacts.js
@@ -0,0 +1,104 @@
+import fs from "fs/promises";
+import path from "path";
+
+const defaultPath = path.resolve("db/contacts.json");
+
+const listContacts = async (contactsPath = defaultPath) => {
+  const contacts = await fs
+    .readFile(contactsPath, { encoding: "utf-8" })
+    .then((data) => JSON.parse(data))
+    .catch((err) => console.log(err.message));
+  return contacts;
+};
+
+const getContactById = async (contactId, contactsPath) => {
+  try {
+    const contactArray = contactsPath
+      ? await listContacts(contactsPath)
+      : await listContacts();
+    const contactById = contactArray.find(
+      (data) => data.id === contactId.toString()
+    );
+    if (!contactById) throw new Error(`This ID doesn't exist`);
+    else return contactById;
+  } catch (err) {
+    return Error(err);
+  }
+};
+
+const removeContact = async (contactId, contactsPath) => {
+  const contactArray = contactsPath
+    ? await listContacts(contactsPath).catch((error) => error)
+    : await listContacts().catch((error) => error);
+  const contactById = await getContactById(contactId);
+
+  if (contactById.name === "Error") return console.log(contactById.message);
+
+  const filteredContact = contactArray.filter(
+    (data) => data.id !== contactId.toString()
+  );
+  await fs
+    .writeFile(
+      contactsPath || defaultPath,
+      JSON.stringify(filteredContact, null)
+    )
+    .catch((error) => {
+      console.log(`Error in writeFile deleteContactById: ${error}`);
+      return error;
+    });
+  return console.log("Delete is completed");
+};
+
+const addContact = async (name, email, phone, contactsPath) => {
+  const contactArray = contactsPath
+    ? await listContacts(contactsPath).catch((error) => error)
+    : await listContacts().catch((error) => error);
+
+  const idSortedArray = contactArray
+    .map((contact) => parseInt(contact.id))
+    .sort((a, b) => a - b);
+  const newId = idSortedArray[idSortedArray.length - 1] + 1;
+
+  const newContact = {
+    id: newId.toString(),
+    name,
+    email,
+    phone,
+  };
+  const newArray = [...contactArray, newContact];
+  console.log(newArray);
+  await fs
+    .writeFile(contactsPath || defaultPath, JSON.stringify(newArray, null))
+    .catch((error) => {
+      console.log(`Error in writeFile addContact: ${error}`);
+      return error;
+    })
+    .then(() => console.log("Save a new contact end with Succes"));
+};
+
+export { listContacts, removeContact, getContactById, addContact };
+// //listContacts();
+
+// const trying = async () => {
+//   // await removeContact(13);
+
+//   await addContact(
+//     "Józek Brzęczyszczykiewicz",
+//     "masdfasdf@m.sadsafsdafsdpl",
+//     "(2055) 2222-225588"
+//   );
+
+//   console.log(await listContacts());
+// };
+// trying();
+
+//listContacts();
+//removeContact(4);
+//listContacts();
+
+//  fs.readFile(contactsPath)
+//    .then((data) => {
+//      const contactArray = JSON.parse(data.toString());
+//      console.log(contactArray.find((data) => data.id === contactId.toString()));
+//    })
+//    .catch((err) => console.log(err.message));
